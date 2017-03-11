6ec8e5a8023a4c4414a1ed72d1ffed063946ee90
bodoralex <bodaleaa@gmail.com>
2017-03-12 00:32:30 +0100

    basic datagenerator methods added

--- greencommitment/greencommitment/DataGenerator.java
+++ greencommitment/greencommitment/DataGenerator.java
@@ -1,8 +1,28 @@
 package greencommitment;
 
+import java.util.Random;
 
 public class DataGenerator { //serialization
 
+	final int minCons;
+	final int maxCons;
+	int cons;
+	Random random;
 
+	public DataGenerator(int minCons, int maxCons) {
+		this.minCons = minCons;
+		this.maxCons = maxCons;
+		this.cons = (int) (minCons + maxCons) / 2;
+		random = new Random();
+	}
+
+	public int getConsuption() {
+		refreshConsuption();
+		return cons;
+	}
+
+	private void refreshConsuption() {
+		cons = random.nextInt(maxCons - minCons) + minCons;
+	}
 
 }
