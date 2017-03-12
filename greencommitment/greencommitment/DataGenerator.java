ae8daefce67848a3c313a7b95f6e1d66e89f1ba0
bodoralex <bodaleaa@gmail.com>
2017-03-12 16:21:19 +0100

    datasending xml refactored

--- greencommitment/greencommitment/DataGenerator.java
+++ greencommitment/greencommitment/DataGenerator.java
@@ -1,22 +1,19 @@
 package greencommitment;
 
-import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Random;
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.*;
+
+import java.util.Date;
+import java.util.Random;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class DataGenerator { // serialization
+	private String address;
 	private final int minCons;
 	private final int maxCons;
 	private int consX;
@@ -26,21 +23,28 @@ public class DataGenerator { // serialization
 	private OutputStream outputStream;
 	private ObjectOutput objectOutput;
 
-	public DataGenerator(int minCons, int maxCons) {
+	public DataGenerator(int minCons, int maxCons, String address) {
+		this.address = address;
 		this.minCons = minCons;
 		this.maxCons = maxCons;
-		this.consX = (int) (minCons + maxCons) / 3;
-		this.consY = (int) (minCons + maxCons) / 3;
 		random = new Random();
 	}
 
 	public static void main(String[] args) throws UnknownHostException, InterruptedException {
 
-		DataGenerator client = new DataGenerator(0, 100000);
+		DataGenerator client = new DataGenerator(0, 100000, "Miskolc nemtom u 69.");
 		client.connectTillSuccess(InetAddress.getLocalHost(), 8020);
+		client.startStreaming();
+	}
+
+	public void startStreaming() {
 		while (true) {
-			client.sendCurrentConsuption();
-			Thread.sleep(1000);
+			sendCurrentConsuption();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -67,9 +71,22 @@ public class DataGenerator { // serialization
 		Element rootelem = doc.createElement("measure");
 		doc.appendChild(rootelem);
 		Element rate = doc.createElement("rate");
-		rate.setAttribute("x", Integer.toString(consX));
-		rate.setAttribute("y", Integer.toString(consX));
+		Element x = doc.createElement("x");
+		x.setTextContent(Integer.toString(consX));
+		rate.appendChild(x);
+		Element y = doc.createElement("y");
+		y.setTextContent(Integer.toString(consY));
+		rate.appendChild(y);
+		Element date = doc.createElement("TimeStamp");
+		date.setTextContent(getDate());
+		rate.appendChild(date);
+
 		return doc;
+	}
+
+	private String getDate() {
+		SimpleDateFormat format = new SimpleDateFormat("yyy.MM.dd HH:mm:ss");
+		return format.format(new Date());
 	}
 
 	public void connectTillSuccess(InetAddress ipAddress, int port) {
@@ -111,12 +128,12 @@ public class DataGenerator { // serialization
 
 	public int[] getConsuption() {
 		refreshConsuption();
-		return new int[]{consX, consY};
+		return new int[] { consX, consY };
 	}
 
 	private void refreshConsuption() {
 		consX = random.nextInt(maxCons - minCons) + minCons;
-		if(maxCons - consX >= minCons){
+		if (maxCons - consX >= minCons) {
 			consY = random.nextInt(maxCons - consX) + minCons;
 		}
 	}
